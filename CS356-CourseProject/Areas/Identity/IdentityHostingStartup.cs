1b4ea94bd10a78a6262b418768f50861a355edc1
Tariq <tkamankwah@captechu.edu>
2021-11-16 12:27:08 -0500

    Added base authentification system.

---
+++ CS356-CourseProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CS356_CourseProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CS356_CourseProject.Areas.Identity.IdentityHostingStartup))]
+namespace CS356_CourseProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CS356_AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CS356_AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CS356_AuthDbContext>();
+            });
+        }
+    }
+}
--- CS356-CourseProject/Startup.cs
+++ CS356-CourseProject/Startup.cs
@@ -44,7 +44,7 @@ namespace CS356_CourseProject
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
